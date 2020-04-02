640027c967980452f4541add77ed7e0772f53257
chh <477@qq.com>
2020-04-02 15:00:29 +0800

    2010
    2020.4.2

--- src/Lecode/_289_生命游戏.java
+++ src/Lecode/_289_生命游戏.java
@@ -6,6 +6,19 @@ public class _289_生命游戏 {
      }
 
     public void gameOfLife(int[][] board) {
+         //该数组用于访问结点的周围8个位置
+         int [] arr = {0,1,-1};
+         //复制一个数组
+        int rows = board.length;//行
+        int cols = board[0].length;//列
+        int[][] copy = new int[rows][cols];
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                copy[i][j] = board[i][j];
+            }
+        }
+
 
     }
 }
---
+++ src/杂项/tt.java
@@ -0,0 +1,21 @@
+package 杂项;
+
+public class tt {
+    public static void main(String[] args) {
+        t t1 = new t();
+        t t2 = new t();
+        t1.i = 1;
+        System.out.println(t2.i);
+    }
+
+}
+
+ class t{
+   int  i = 0;
+}
+
+
+//class t{
+//    static int  i = 0;
+//}
+
